e8338a2dfd06cb5e989092f0ad5e5a8ac69e4a73
Shakhzod235 <shakhzod235@mail.ru>
2022-07-23 12:10:39 +0500

    fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const submit = document.querySelector('.review__btn');
-const thankYou = document.querySelector('.thank-you');
+const thankYou = document.querySelector('.gratitude');
 const review = document.querySelector('.review');
 submit.addEventListener("click", ()=> {
     thankYou.classList.toggle('active');
     review.classList.toggle('disable');
 })
-const textPlaceholder = document.querySelector('.thank-you__rate');
+const textPlaceholder = document.querySelector('.gratitude__rate');
